cc77459af0811016139fd06da39e2c03ae0880bf
Augusto <gutocoding@gmail.com>
2026-04-17 19:40:10 -0300

    feat: conclui pratica04. Fecha #ID

---
+++ praticas/praticas04/ordenacao.h
@@ -0,0 +1,10 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+void bubble_sort(int v[], int tamanho);
+void selection_sort(int v[], int tamanho);
+void quick_sort(int v[], int inicio, int fim);
+void sortear(int v[], int tamanho);
+void imprimir(int v[], int tamanho);
+
+#endif
