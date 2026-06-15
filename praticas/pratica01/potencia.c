6d20154a6acfc4612748baf57d08243ff630c2eb
gutocoding <gutocoding@gmail.com>
2026-06-15 15:43:50 -0300

    fix: resolve merge conflict in potencia.c

--- praticas/pratica01/potencia.c
+++ praticas/pratica01/potencia.c
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int calcularpotencia(int base, int expoente)
-{
-    int temp = base;
-    if (base <= 0 || expoente <= 0)
-    {
-        printf("Base ou expoente inválidos (use numeros inteiros positivos\n)");
-        return 0;
-    }
-    for (int i = 1; i < expoente; i++)
-    {
-        temp = base * temp;
-    }
-
-    return temp;
-}
-
-int main()
-{
-    int base = 4;
-    int expoente = 2;
-    int resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-
-    base = 2;
-    expoente = 3;
-    resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-
-    base = 2;
-    expoente = 5;
-    resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-
-    base = 10;
-    expoente = 0;
-    resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-
-    base = -3;
-    expoente = 2;
-    resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-
-    base = 2;
-    expoente = -2;
-    resultado = calcularpotencia(base, expoente);
-    printf("Resultado: %d\n", resultado);
-    return 0;
-=======
 #include <stdio.h>
 
 int calcpotencia(int base, int expoente)
@@ -78,5 +26,4 @@ int main()
     printf("Base 0, expoente 2: %d\n", calcpotencia(0,2));
     printf("Base -2, expoente 2: %d\n", calcpotencia(-2,2));
     return 0;
->>>>>>> ee8d4fa4b81bd1eca8d07fde0ac8d872ba5af4ce
 }
